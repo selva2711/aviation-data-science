e62b042816cf972ed38c7ad3b60e1f27bc115579
Selva Ancy <selva2711992@gmail.com>
2026-09-11 15:46:42 +0530

    Add initial Python script for airline data

---
+++ 01_python/day01_variables.py
@@ -0,0 +1,14 @@
+# Day 1 - Python Variables
+# Aviation Data Science Learning Journey
+
+airline = "Qatar Airways"
+flight_number = "QR170"
+origin = "Doha"
+destination = "Mumbai"
+passengers = 200
+
+print(airline)
+print(flight_number)
+print(origin)
+print(destination)
+print(passengers)
