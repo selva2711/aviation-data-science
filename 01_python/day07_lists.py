1d34faf469b78c855f9e0f7cb91fcc4cd66befdc
Selva Ancy <selva2711992@gmail.com>
2026-09-18 15:28:16 +0530

    Add Python script for managing flight numbers
    This script demonstrates basic list operations in Python, including appending a new flight number and printing the list and its length.

---
+++ 01_python/day07_lists.py
@@ -0,0 +1,17 @@
+# Day 7 - Python Lists
+
+flight_numbers = ["QR170", "QR500", "QR700"]
+
+print("First Flight:", flight_numbers[0])
+
+flight_numbers.append("QR900")
+
+print("All Flights:", flight_numbers)
+
+print("Total Flights:", len(flight_numbers))
+
+# Output
+
+First Flight: QR170
+All Flights: ['QR170','QR500','QR700','QR900']
+Total Flights: 4
