91d48da53df854193367b7d04526bb858cc64342
Selva Ancy <selva2711992@gmail.com>
2026-09-21 14:24:30 +0530

    Add Python script for flight information using tuples
    This script demonstrates the use of tuples in Python by storing and printing flight information.

---
+++ 01_python/day10_tuples.py
@@ -0,0 +1,19 @@
+# Day 10 - Python Tuples
+
+flight_info = ("QR170", "Doha", "Mumbai")
+
+print("Flight Info:", flight_info)
+
+print("Flight Number:", flight_info[0])
+print("Origin:", flight_info[1])
+print("Destination:", flight_info[2])
+
+print("Total Items:", len(flight_info))
+
+# Output
+
+Flight Info: ('QR170', 'Doha', 'Mumbai')
+Flight Number: QR170
+Origin: Doha
+Destination: Mumbai
+Total Items: 3
